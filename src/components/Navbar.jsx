2a51164cedadafbcbbf1fa9afde13ac60816b3fa
João Victor Magrão <joaovmagrao10@gmail.com>
2023-01-08 15:57:36 -0300

    Pagina home criada

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+
+import './Navbar.css'
+
+const Navbar = () => {
+  const [search, setSearch] = useState('')
+
+  return (
+    <nav id='navbar'>
+      <h2>
+        <Link to='/'>
+          <BiCameraMovie /> Movies Lib
+        </Link>
+      </h2>
+      <form>
+        <input type='text' placeholder='Busque um filme' />
+        <button type='submit'>
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Search.jsx
@@ -0,0 +1,5 @@
+const Search = () => {
+  return <div>Home</div>
+}
+
+export default Search
