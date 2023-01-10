52e93754394a19d078e303af0f051d32b3292837
João Victor Magrão <joaovmagrao10@gmail.com>
2023-01-09 21:06:25 -0300

    Versão final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 import './Navbar.css'
 
 const Navbar = () => {
   const [search, setSearch] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
 
   return (
     <nav id='navbar'>
@@ -14,8 +23,13 @@ const Navbar = () => {
           <BiCameraMovie /> Movies Lib
         </Link>
       </h2>
-      <form>
-        <input type='text' placeholder='Busque um filme' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Busque um filme'
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type='submit'>
           <BiSearchAlt2 />
         </button>
